3240dcdfa4efd02b308c3a86ebc139d8d04e4425
Vinicius Freitas <viniciusscfjr@gmail.com>
2023-09-12 17:57:36 -0300

    init project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Exercício do Curso de Elixir
+
+## Requisitos do projeto
+
+- Vamos ler uma lista de números de um arquivo, caso o arquivo não exista deve exibir uma mensagem de erro.
+- Múltiplos de 3 devem retornar Fizz
+- Múltiplos de 5 devem retornar Buzz
+- Múltiplos de 3 e 5 devem retornar FizzBuzz
+
+Ex.
+
+[1,2,3,4,5,15] => [1,2,Fizz,4,Buzz,FizzBuzz] \
+[1,2,4,8,10] => [1,2,4,8,Buzz]
---
+++ lib/fizzbuzz.ex
@@ -0,0 +1,41 @@
+defmodule FizzBuzz do
+  @moduledoc """
+  - Vamos ler uma lista de números de um arquivo, caso o arquivo não exista deve exibir uma mensagem de erro.
+  - Múltiplos de 3 devem retornar Fizz
+  - Múltiplos de 5 devem retornar Buzz
+  - Múltiplos de 3 e 5 devem retornar FizzBuzz
+
+  Exemplo:
+
+  [1,2,3,4,5,15] => [1,2,Fizz,4,Buzz,FizzBuzz] \
+  [1,2,4,8,10] => [1,2,4,8,Buzz]
+  """
+  def build(file_name) do
+    file_name
+    |> File.read()
+    |> handle_file_read()
+  end
+
+  defp handle_file_read({:ok, result}) do
+    result =
+      result
+      |> String.split(",")
+      |> Enum.map(&convert_and_evaluate_numbers/1)
+      
+    {:ok, result}
+  end
+
+  defp handle_file_read({:error, reason}), do: {:error, "Error reading the file: #{reason}"}
+
+  defp convert_and_evaluate_numbers(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_numbers()
+  end
+
+  defp evaluate_numbers(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_numbers(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_numbers(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_numbers(number), do: number
+
+end
